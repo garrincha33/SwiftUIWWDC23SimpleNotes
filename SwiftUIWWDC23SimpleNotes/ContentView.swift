03d411be8de1a4a01feaba8cd553145cbff56276
Richy <garrincha33@users.noreply.github.com>
2023-07-05 09:03:24 +0100

    Initial Commit

---
+++ SwiftUIWWDC23SimpleNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIWWDC23SimpleNotes
+//
+//  Created by Richard Price on 05/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIWWDC23SimpleNotes/SwiftUIWWDC23SimpleNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWWDC23SimpleNotesApp.swift
+//  SwiftUIWWDC23SimpleNotes
+//
+//  Created by Richard Price on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWWDC23SimpleNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
